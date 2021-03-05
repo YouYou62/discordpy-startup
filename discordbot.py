44c4b48aa5d31a99aa36c0f7199e58d340b7658c
YouYou62 <80115534+YouYou62@users.noreply.github.com>
2021-03-06 01:48:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -19,7 +20,7 @@ async def vc(ctx):
      userNAME = []
 
      #ユーザーIDの取得
-     user_id = message.author.voice.channel.voice_states.keys()
+     user_id = ctx.author.voice.channel.voice_states.keys()
      for users in user_id:
         print(users)
      userID += user_id
@@ -29,20 +30,20 @@ async def vc(ctx):
      ID_len = len(userID)
      i = 0
      while i < ID_len:
-         ID = str(userID[int(i)])
-         user = await client.fetch_user(ID)
-         MESSAGE = user.name, user.discriminator, str(user)
-         userNAME.insert(0, MESSAGE[0])
-         i += 1
+        ID = str(userID[int(i)])
+        user = await client.fetch_user(ID)
+        MESSAGE = user.name, user.discriminator, str(user)
+        userNAME.insert(0, MESSAGE[0])
+        i += 1
                 
      #ブキルーレットのチャット
      i = 0
      while i < len(userNAME):
         r = random.randint(1, 129)
-        await message.channel.send("**" + str(userNAME[i]) + "**さんは**__" + str(Buki_all[int(r)]) + "__**でし！")
+        await ctx.send("**" + str(userNAME[i]) + "**さんは**__" + str(Buki_all[int(r)]) + "__**でし！")
         i += 1
         
 async def buki(ctx):
-    await ctx.send("でしでし！")
+    await ctx.send('でしでし！')
 
 bot.run(token)
